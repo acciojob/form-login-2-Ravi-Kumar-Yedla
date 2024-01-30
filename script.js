cb9d4e53e5020ca9d41bb3aa4b0a2d82d57fa8c5
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-01-30 10:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function submitForm(){
+	//collect all the values from the above form
+      var first =  document.getElementById("firstName").v;	
+      var last =  document.getElementById("lastName");	
+      var num=  document.getElementById("phoneNumber");	
+      var email =  document.getElementById("EmailId");	
+
+//create a message
+var message=
+	"First Name:" +firstName+"\n"+
+	"Last Name:" +lastName+"\n"+
+	"Phone Number:"+phoneNumber+"\n"+
+	"Email ID:"+EmailId+"\n"+
+alert(message);
+	
+
+}
+
+
+	
+	
